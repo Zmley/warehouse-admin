c945c42ae920cb4876b50ac7d0a2b7f3bfbff44c
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-10 17:43:02 -0300

    feat:delete server

--- server/src/routes/products/product.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProducts } from "./product.controller";
-
-const router = express.Router();
-
-router.get("/", getProducts);
-
-export default router;
--- server/src/routes/products/product.service.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "./product.model";
-
-export const getProductCodes = async (): Promise<string[]> => {
-  const products = await Product.findAll({
-    attributes: ["productCode"],
-    raw: true,
-  });
-  return products.map((p) => p.productCode);
-};
