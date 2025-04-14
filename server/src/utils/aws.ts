fcfeb977e195a8ebbafa414f560a28e9e2d0c5c1
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-14 12:10:20 -0300

    delete： delete server file

--- server/src/utils/aws.ts
+++
@@ -1,9 +0,0 @@
-import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider'
-
-export const cognitoClient = new CognitoIdentityProviderClient({
-  region: process.env.AWS_REGION!
-})
-
-export const getCognitoPublicKeysUrl = (userPoolId: string, region: string) => {
-  return `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`
-}
