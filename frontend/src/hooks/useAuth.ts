d84222da79cabebd1c00f6c6940aaa55a40f7db3
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-14 12:02:51 -0300

    feat: refactor code

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,39 +1,39 @@
-import { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../contexts/auth'
-import { loginUser } from '../api/authApi'
-import { saveTokens, clearTokens } from '../utils/Storages'
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/auth";
+import { loginUser } from "../api/authApi";
+import { saveTokens, clearTokens } from "../utils/Storages";
 
 export const useAuth = () => {
   const { isAuthenticated, setIsAuthenticated, userProfile } =
-    useContext(AuthContext)!
-  const [error, setError] = useState<string | null>(null)
-  const navigate = useNavigate()
+    useContext(AuthContext)!;
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const handleLogin = async (email: string, password: string) => {
-    setError(null)
+    setError(null);
     try {
-      const tokens = await loginUser(email, password)
-      saveTokens(tokens)
-      setIsAuthenticated(true)
+      const tokens = await loginUser(email, password);
+      saveTokens(tokens);
+      setIsAuthenticated(true);
 
-      navigate('/')
+      navigate("/");
     } catch (err: any) {
       console.error(
-        '❌ Login Error:',
-        err.response?.data?.message || 'Unknown error'
-      )
+        "❌ Login Error:",
+        err.response?.data?.message || "Unknown error"
+      );
       setError(
-        err.response?.data?.message || '❌ Login failed due to unknown error.'
-      )
+        err.response?.data?.message || "❌ Login failed due to unknown error."
+      );
     }
-  }
+  };
 
   const handleLogout = () => {
-    clearTokens()
-    setIsAuthenticated(false)
-    navigate('/')
-  }
+    clearTokens();
+    setIsAuthenticated(false);
+    navigate("/");
+  };
 
-  return { handleLogin, handleLogout, isAuthenticated, error, userProfile }
-}
+  return { handleLogin, handleLogout, isAuthenticated, error, userProfile };
+};
