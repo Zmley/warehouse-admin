8a27ef5bc68345d4699a5ea0499d3a877112c34c
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-14 14:34:49 -0300

    feat: refactor code

--- frontend/src/api/taskApi.ts
+++ frontend/src/api/taskApi.ts
@@ -1,6 +1,7 @@
 import apiClient from './axiosClient.ts'
 
 export const fetchTasks = async (warehouseID: string) => {
+  //  using /tasks/all temporaryly due to conflic with other '/tasks'
   const response = await apiClient.get('/tasks/all', {
     params: { warehouseID }
   })
@@ -12,6 +13,7 @@ export const createTask = async (payload: {
   destinationBinCode: string
   productCode: string
 }) => {
+  //  using /tasks/admin temporaryly due to conflic with other '/tasks'
   const response = await apiClient.post('/tasks/admin', payload)
   return response.data
 }
