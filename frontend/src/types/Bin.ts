6208f36687210a39ff194160d571fe2883ee9935
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-25 16:47:25 -0300

    refactor ：refactor code base on pr from jiabin

--- frontend/src/types/Bin.ts
+++ frontend/src/types/Bin.ts
@@ -1,6 +1,8 @@
+import { BinType } from 'constants/binTypes'
+
 export interface Bin {
   binID: string
   binCode: string
-  type: 'INVENTORY' | 'PICK_UP' | 'CART'
+  type: BinType
   defaultProductCodes: string | null
 }
