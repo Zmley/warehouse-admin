2118c050267844750a00741cd95669f5eab6a2fb
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-05-02 15:17:12 -0300

    refactor: deploy

--- frontend/src/utils/Storages.ts
+++ frontend/src/utils/Storages.ts
@@ -15,7 +15,6 @@ export const getAccessToken = (): string | null => {
 export const clearTokens = () => {
   localStorage.clear()
 }
-//test
 export const areTokensValid = (): boolean => {
   return !!(
     localStorage.getItem('accessToken') &&
