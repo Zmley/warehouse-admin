ddebc768c0511a508875f5aed0ab8c7b8a67a7a5
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-11-18 10:03:49 -0400

    fix： fix issue

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -9,32 +9,32 @@ import {
 } from 'types/auth.js'
 
 export const loginUser = async (email: string, password: string) => {
-  const response = await apiClient.post('/login', { email, password })
+  const response = await apiClient.post('/account/login', { email, password })
   return response.data
 }
 
 export const fetchUserProfile = async () => {
-  const response = await apiClient.get('/me')
+  const response = await apiClient.get('/account/me')
   return response.data || null
 }
 
 export async function getWorkerNames(): Promise<WorkerName[]> {
-  const res = await apiClient.get<NamesResponse>('/names')
+  const res = await apiClient.get<NamesResponse>('/account/names')
   return res.data?.workers ?? []
 }
 
 export const createEmployee = async (
   payload: CreateEmployeePayload
 ): Promise<CreateEmployeeResponse> => {
-  const res = await apiClient.post('/register', payload)
+  const res = await apiClient.post('/account/register', payload)
   return res.data
 }
 
 export const getAllEmployees = async (): Promise<Employee[]> => {
-  const res = await apiClient.get<GetEmployeesResponse>('/all')
+  const res = await apiClient.get<GetEmployeesResponse>('/account/all')
   return res.data.accounts
 }
 
 export const deleteEmployeeAPI = (accountID: string) => {
-  return apiClient.delete(`/${accountID}`)
+  return apiClient.delete(`/account/${accountID}`)
 }
